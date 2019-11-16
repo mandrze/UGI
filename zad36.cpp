1a93a5f312737d89bf01af940288610583017a45
Mariusz Andrzejewski <32964981+mandrze@users.noreply.github.com>
2019-11-16 17:09:23 +0100

    Add files via upload

--- zad36.cpp
+++ zad36.cpp
@@ -5,32 +5,23 @@ int main()
 {
 	char  tab[3], odp[3];
 	char tab1[]="bba";
-	cout<<"Podaj wynik: 2*2= \n a) 3\nb) 4\n";
+	cout<<"Podaj wynik: 2*2= \na) 3\nb) 4\n";
 	cin>>tab[0];
-	cout<<"2";
+	cout<<"Podaj wynik 8/2= \na) 7\nb) b\n";
 	cin>>tab[1];
-	cout<<"3";
+	cout<<"Podaj wynik 7+8= \na) 15\nb) 11\n";
 	cin>>tab[2];
 	for (int i=0; i<3;i++)
 	{
 		if (tab[i]==tab1[i]) odp[i]='1';
 		if (tab[i]!=tab1[i]) odp[i]='0';
-		cout<<tab[i]<<" "<<odp[i]<<"\n";
+		if (odp[i]==0) cout<<"Zadanie "<<i;
 	}
+	
 	
 
 
 	return 0;
 	
 }
-//int liczby[ 3 ];
-//    std::cout << "Podaj liczbe 1:";
-//    std::cin >> liczby[ 0 ];
-//    std::cout << "Podaj liczbe 2:";
-//    std::cin >> liczby[ 1 ];
-//    liczby[ 2 ] = 567;
-//    std::cout << "liczby[2] wynosi " << liczby[ 2 ] << std::endl;
-//    int suma = liczby[ 0 ] + liczby[ 1 ] + liczby[ 2 ];
-//    std::cout << "Suma trzech liczb wynosi:" << suma << std::endl;
-//
 
