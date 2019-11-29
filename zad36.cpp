aad585da2a0f1925f415fc064f674151c2fb0a9e
Mariusz Andrzejewski <32964981+mandrze@users.noreply.github.com>
2019-11-29 19:04:12 +0100

    Add files via upload

--- zad36.cpp
+++ zad36.cpp
@@ -1,27 +1,39 @@
 #include <iostream>
+#include <stdio.h>
 using namespace std;
 
 int main()
 {
-	char  tab[3], odp[3];
-	char tab1[]="bba";
-	cout<<"Podaj wynik: 2*2= \na) 3\nb) 4\n";
-	cin>>tab[0];
-	cout<<"Podaj wynik 8/2= \na) 7\nb) b\n";
-	cin>>tab[1];
-	cout<<"Podaj wynik 7+8= \na) 15\nb) 11\n";
-	cin>>tab[2];
-	for (int i=0; i<3;i++)
-	{
-		if (tab[i]==tab1[i]) odp[i]='1';
-		if (tab[i]!=tab1[i]) odp[i]='0';
-		if (odp[i]==0) cout<<"Zadanie "<<i;
+	
+	int lenght = 3;
+	double  tab[lenght];
+	double odpp, odpn;
+	double sum = 0;
+	double odp[] = {4, 4, 15};
+	char znak;
+	do {
+		cout<<"Podaj wynik 2*2= \n";
+		cin>>tab[0];
+		cout<<"Podaj wynik 8/2= \n";
+		cin>>tab[1];
+		cout<<"Podaj wynik 7+8= \n";
+		cin>>tab[2];
+		for (int i=0; i<3;i++)
+		if (tab[i]==odp[i]){ sum+=1;
 	}
+		else {cout<<"Odpowiedziales zle na pytanie "<<i+1<<"\n";
+	}
+		odpp=(sum/lenght)*100;
+		odpn=100-odpp;
+		cout<<"Udzieliles "<<odpp<<"% poprawnych odpowiedzi i "<<odpn<<"% niepoprawnych odpowiedzi ";
+		cout<<"Czy chcesz powtorzyc test? t/n";
+		cin>>znak;
 	
 	
+	} while(znak =='t');
+
 
 
 	return 0;
 	
 }
-
